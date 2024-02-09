1268fec923ac1f690301551bc3e69a7d7fdcd75b
Kwiniu 18 <dom.kwintal@interia.pl>
2024-02-09 01:32:07 +0100

    working at redirecting

--- main.py
+++ main.py
@@ -2,38 +2,46 @@ from typing import Union, Annotated
 
 from fastapi import FastAPI, Depends
 
+from fastapi.responses import RedirectResponse
+
 from pydantic import BaseModel
 
 import shortuuid
 
 
 app = FastAPI()
-urlDict = dict()
+url_list = {
+    "uuid": "https://typer.tiangolo.com"
+}
 
 @app.get("/")
 async def root():
     return {"message": "Hello World"}
 
 
-def uuid_maker(x):
-    link = x
+def make_url(user_url):
     pre = "127.0.0.1:8000/"
-    uuid = shortuuid.ShortUUID().random(length=5)
-    shorter = pre + uuid
-    urlDict[shorter] = link
+    url_list[shorter] = user_url
     return {
         
-    "short-url":shorter, 
-    "link": link
+    "short-url":(pre + (shortuuid.ShortUUID().random(length=5))) , 
+    "link": user_url
     
     }
 
 
 def item(url: str):
-    return uuid_maker(url)
+    return make_url(url)
 
 
 @app.post("/shortie/{url}")
 async def short_url(shortie: Annotated[dict, Depends(item)]):
     return shortie
 
+@app.get("/get/{uuid}")
+def get_url(uuid: str):
+    print(url_list)
+    if uuid in url_list:
+        return RedirectResponse(
+            url = (url_list[uuid])
+            )
